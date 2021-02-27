d6245c82a31dce52c10bdb7e770858896da1d755
Francisco Lopez <franlopbri@gmail.com>
2021-02-26 20:21:26 -0600

    creando hola mundo

---
+++ src/main/java/com/franlops/market/FranlopsMarketApplication.java
@@ -0,0 +1,13 @@
+package com.franlops.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranlopsMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranlopsMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franlops/market/FranlopsMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franlops.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranlopsMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
