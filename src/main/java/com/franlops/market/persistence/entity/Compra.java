679a46957896a89d03506bd443f31b32773fac18
Francisco Lopez <franlopbri@gmail.com>
2021-03-09 17:10:32 -0600

    definiendo clases para Mapear productos y compras

---
+++ src/main/java/com/franlops/market/persistence/entity/Compra.java
@@ -0,0 +1,73 @@
+package com.franlops.market.persistence.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "compras")
+public class Compra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_cliente")
+    private String id_Cliente;
+
+    private LocalDateTime fecha;
+
+    @Column(name = "medio_pago")
+    private String medioPago;
+
+    private String comentario;
+    private String estado;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getId_Cliente() {
+        return id_Cliente;
+    }
+
+    public void setId_Cliente(String id_Cliente) {
+        this.id_Cliente = id_Cliente;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(String medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
