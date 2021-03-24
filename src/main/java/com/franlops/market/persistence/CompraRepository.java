6be692d6d80ce124004e6198bf59d2cdf498bab8
Francisco Lopez <franlopbri@gmail.com>
2021-03-23 23:02:05 -0600

    Crear el repositorio de compras

---
+++ src/main/java/com/franlops/market/persistence/CompraRepository.java
@@ -0,0 +1,41 @@
+package com.franlops.market.persistence;
+
+import com.franlops.market.domain.Purchase;
+import com.franlops.market.domain.repository.PurchaseRepository;
+import com.franlops.market.persistence.crud.CompraCrudRepository;
+import com.franlops.market.persistence.entity.Compra;
+import com.franlops.market.persistence.mapper.PurchaseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CompraRepository implements PurchaseRepository {
+
+    @Autowired
+    private CompraCrudRepository compraCrudRepository;
+
+    @Autowired
+    private PurchaseMapper mapper;
+
+    @Override
+    public List<Purchase> getAll() {
+        return mapper.toPurchases((List<Compra>) compraCrudRepository.findAll());
+    }
+
+    @Override
+    public Optional<List<Purchase>> getByClient(String clientId) {
+        return compraCrudRepository.findByIdCliente(clientId)
+                .map(compras -> mapper.toPurchases(compras));
+    }
+
+    @Override
+    public Purchase save(Purchase purchase) {
+        Compra compra = mapper.toCompra(purchase);
+        compra.getProductos().forEach(producto -> producto.setCompra(compra));
+
+        return mapper.toPurchase(compraCrudRepository.save(compra));
+    }
+}
