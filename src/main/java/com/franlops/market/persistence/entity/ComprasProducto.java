a7ed2ef9f808c7bde5e60a52b8e889dbc3af04e4
Francisco Lopez <franlopbri@gmail.com>
2021-03-25 21:28:31 -0600

    se corrige nombre de la tabla en ComprasProducto Entity

--- src/main/java/com/franlops/market/persistence/entity/ComprasProducto.java
+++ src/main/java/com/franlops/market/persistence/entity/ComprasProducto.java
@@ -3,7 +3,7 @@ package com.franlops.market.persistence.entity;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "compras_producto")
+@Table(name = "compras_productos")
 public class ComprasProducto {
 
     @EmbeddedId
