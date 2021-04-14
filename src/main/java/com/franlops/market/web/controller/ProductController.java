28081da477b162ebd883e27d46d5cc415eba139a
Francisco Lopez <franlopbri@gmail.com>
2021-04-13 19:39:57 -0500

    Actualizaciones para Swagger UI

--- src/main/java/com/franlops/market/web/controller/ProductController.java
+++ src/main/java/com/franlops/market/web/controller/ProductController.java
@@ -2,10 +2,7 @@ package com.franlops.market.web.controller;
 
 import com.franlops.market.domain.Product;
 import com.franlops.market.domain.service.ProductService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +18,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/all")
-    @ApiOperation("Get all supermarket products")
+    @ApiOperation(value = "Get all supermarket products", authorizations = { @Authorization(value = "JWT")})
     @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<Product>> getAll(){
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
